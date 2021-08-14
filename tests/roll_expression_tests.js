331751b6abbe5c2873c49a0996009a31edb7743c
Mitten.O <Mitten.O@gmail.com>
2021-08-14 22:22:35 +0300

    Create a very minimal testing framework

--- tests/roll_expression_tests.js
+++ tests/roll_expression_tests.js
@@ -7,6 +7,44 @@
 
 //var auxMeth = require( '../module/auxmeth.js' );
 
+import process from 'process'
 import { auxMeth } from '../module/auxmeth.js'
 
-console.log( 'Runs' );
+// Colored output variables.
+var color_reset = "\x1b[0m"
+var color_red = "\x1b[31m"
+var color_green = "\x1b[32m"
+var color_yellow = "\x1b[33m"
+
+// Simple test assertion.
+var stop_on_fail = false;
+var successful_asserts = 0;
+var failed_asserts = 0;
+function assert( success, message ){
+
+    if( success ){
+        // Report success.
+        console.log( `${message}${color_green}\tSuccess${color_reset}` );
+        successful_asserts++;
+    } else {
+        // Report failure.
+        console.log( `${message}${color_red}\Failure${color_reset}` );
+        failed_asserts++;
+    }
+
+}
+
+function test_basic_arithmetic(){
+    assert( true, "Test truth" );
+}
+test_basic_arithmetic();
+
+console.log( `Successful asserts: ${successful_asserts}` );
+console.log( `Failed asserts: ${failed_asserts}` );
+
+if( failed_asserts == 0 ){
+    console.log( `${color_green}Total Success!${color_reset}` );
+} else {
+    console.log( `${color_red}Tests failed!${color_reset}` );
+    process.exitCode = failed_asserts;
+}
