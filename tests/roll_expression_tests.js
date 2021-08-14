12dc16e0b117a7ab969bf65fa6f4862841d93e30
Mitten.O <Mitten.O@gmail.com>
2021-08-14 22:08:06 +0300

    Create testing file and a package.json to be able to use import syntax

---
+++ tests/roll_expression_tests.js
@@ -0,0 +1,12 @@
+// This file can be run using node.js to test
+// the roll expression parser.
+// It is a complex piece of code, so it
+// benefits from being tested programmatically without
+// having to mess around int the ui.
+
+
+//var auxMeth = require( '../module/auxmeth.js' );
+
+import { auxMeth } from '../module/auxmeth.js'
+
+console.log( 'Runs' );
